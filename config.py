871787b15354179b0be7a7c30c6a5e9ca73f67f3
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2024-04-04 12:03:08 +0530

    Update config.py
    Signed-off-by: Devilking12 <128044704+Devilking12@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/Assaulted_dark",
+    "https://github.com/Devilking12/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
