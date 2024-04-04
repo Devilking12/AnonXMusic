4fa023dfeb23c38379e91b8326481726f382eb1d
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2024-04-04 11:27:47 +0530

    Update start.py
    Signed-off-by: Devilking12 <128044704+Devilking12@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,12 +26,12 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.6791925138),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.https://t.me/Devil_support_network),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.https://t.me/feelings_for_you_baby),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.https://graph.org/file/029bf1df4f93265764cf2.mp4),
         ],
     ]
     return buttons
